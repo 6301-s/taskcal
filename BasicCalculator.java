f98e922cddb7bba9fcb7a723627f5d1cb1a6bd4e
6301-s <147502761+6301-s@users.noreply.github.com>
2023-10-31 18:28:47 +0530

    Create BasicCalculator.java

---
+++ BasicCalculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        try {
+            System.out.print("Enter the first number: ");
+            double num1 = scanner.nextDouble();
+            
+            System.out.print("Enter the operation (+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
+            
+            System.out.print("Enter the second number: ");
+            double num2 = scanner.nextDouble();
+            
+            double result ;
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                	double num3;
+                	try {
+                    if (num2 == 0) {
+                        throw new ArithmeticException();
+                        
+                    }
+                    num3=num1/num2;
+                    System.out.println(num3);
+                	}
+                    catch(ArithmeticException e) {
+                    	System.out.println("Division by zero is not allowed");
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("Error: Invalid operator.");
+                    return;
+            
+            }
+
+            System.out.println(result);
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("Error: Invalid input. Please enter valid numbers.");
+        }
+    }
+}
